d30d0ea227d8375b1ec3e2636a494a9503653aa8
emi <137839945+Emii09@users.noreply.github.com>
2025-06-20 19:44:01 -0300

    Update bmi.c

--- bmi.c
+++ bmi.c
@@ -5,10 +5,30 @@ int main()
 	float peso, estatura, imc;
 	
 	printf("--- Calculadora de Indice De Masa Corporal ---");
+	printf("\n");
+	
 	printf("\nEscriba su peso en kg: ");
 	scanf ("%f", &peso);
+	if (peso<=0){
+		while (peso<=0){
+			printf("Por favor, ingrese un valor valido.");
+			printf("\nEscriba su peso en kg: ");
+			scanf ("%f", &peso);
+			printf("\n");
+		}
+	}
+	
 	printf ("Bien!, ahora escriba su estatura en metros: ");
 	scanf ("%f", &estatura);
+	if (estatura<=0){
+		while (estatura<=0){
+			printf("Por favor, ingrese un valor valido.");
+			printf("\nEscriba su estatura en metros: ");
+			scanf ("%f", &estatura);
+			printf("\n");
+		}
+	}
+	
 	imc = peso/(estatura*estatura);
 	printf("\nSu Indice de Masa Corporal es: %.2f\n ", imc);
 	printf("\nGuiese por la siguiente tabla.");
